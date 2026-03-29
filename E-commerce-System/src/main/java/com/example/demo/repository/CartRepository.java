eea1c57e410cf9dc5b8d8728513c9b9d4d034acf
KhushiThakur0123 <thakurkhushi065@gmail.com>
2026-03-29 21:34:14 +0530

    Delete E-commerce-System directory

--- E-commerce-System/src/main/java/com/example/demo/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Cart;
-import com.example.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUser(User user);
-}
